5ea0fd45fa92fdc1c70497529663eedfbccc08fa
Andrew T. Wang <andrew_aw7@yahoo.com>
2016-10-28 22:22:57 -0400

    Update requirements
    Remove deprecated Django features

--- web/urls.py
+++ web/urls.py
@@ -27,7 +27,7 @@
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from web import views
 
